092be453dd5e61ff6ed4102ac34be0274c9fd15e
hichamissoual <hichamissoual.one@gmail.com>
2025-02-23 18:16:58 +0000

    add morgan logger

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // import modules
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 // using modules
 dotenv.config();
 const PORT = process.env.PORT;
 const app = express();
 // body parses
 app.use(express.json());
+// loger midllewars
+app.use(morgan("dev"));
 app.get("/", (req, res) => {
   res.status(200).json({
     status: "Success",
