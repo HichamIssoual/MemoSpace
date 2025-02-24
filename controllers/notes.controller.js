5b1ad935193f4ace212f1319f160c2cfe4edc721
hichamissoual <hichamissoual.one@gmail.com>
2025-02-24 08:54:51 +0000

    routes controllers and models and connection db

--- controllers/notes.controller.js
+++ controllers/notes.controller.js
@@ -1,10 +1,86 @@
 const handleAsyncError = require("../middlewares/handle.async.error");
+const NoteModel = require("../models/note.model");
+const errorGenerator = require("../utils/error.generator");
+const { ERROR, SUCCESS, FAIL } = require("./../utils/res.status.text");
+const getAllNotes = handleAsyncError(async (req, res, next) => {
+  const allNotes = await NoteModel.find(
+    {},
+    "userId title description color createAt"
+  );
+  res.status(200).json({
+    status: SUCCESS,
+    data: {
+      notes: allNotes,
+    },
+  });
+});
+const getSingleNote = handleAsyncError(async (req, res, next) => {
+  const noteId = req.params.id;
+  const singleNote = await NoteModel.findById(noteId);
+  if (!singleNote) {
+    const error = errorGenerator.generate("Note Note Found", 404, FAIL);
+    return next(error);
+  }
+  res.status(200).json({
+    status: SUCCESS,
+    data: {
+      note: singleNote,
+    },
+  });
+});
+const addNote = handleAsyncError(async (req, res, next) => {
+  const { userId, title, description, color } = req.body;
 
-const getAllNotes = handleAsyncError(async (req, res, next) => {});
-const getSingleNote = handleAsyncError(async (req, res, next) => {});
-const addNote = handleAsyncError(async (req, res, next) => {});
-const updateNote = handleAsyncError(async (req, res, next) => {});
-const deleteNote = handleAsyncError(async (req, res, next) => {});
+  const newNote = new NoteModel({
+    userId: userId,
+    title: title,
+    description: description,
+    color: color,
+  });
+  await newNote.save();
+  res.status(201).json({
+    status: SUCCESS,
+    message: "Notes created successfully",
+    data: {
+      note: newNote,
+    },
+  });
+});
+const updateNote = handleAsyncError(async (req, res, next) => {
+  const noteId = req.params.id;
+  const currentNote = await NoteModel.findById(noteId);
+  if (!currentNote) {
+    const error = errorGenerator.generate("Note Note Found", 404, FAIL);
+    return next(error);
+  }
+  const updateNote = await NoteModel.findOneAndUpdate(
+    noteId,
+    {
+      $set: { ...req.body },
+    },
+    { new: true, runValidators: true }
+  );
+  res.status(200).json({
+    status: SUCCESS,
+    message: "Note updated successfully",
+    data: {
+      updatedNote: updateNote,
+    },
+  });
+});
+const deleteNote = handleAsyncError(async (req, res, next) => {
+  const noteId = req.params.id;
+  const currentNote = await NoteModel.findById(noteId);
+  if (!currentNote) {
+    const error = errorGenerator.generate("Note Note Found", 404, FAIL);
+    return next(error);
+  }
+  await NoteModel.deleteOne(noteId);
+  res.status(200).json({
+    status: SUCCESS,
+    message: "Note Deleted successfully",
+  });
+});
 module.exports = {
   getAllNotes,
   getSingleNote,
--- models/note.model.js
+++ models/note.model.js
@@ -1,3 +1,27 @@
 const mongoose = require("mongoose");
 
-const noteSchema = "";
+const noteSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: [true, "must add user Id"],
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
+    required: true,
+    default: "#fff",
+  },
+  createAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+module.exports = mongoose.model("notes", noteSchema);
